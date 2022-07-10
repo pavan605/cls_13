e9f6eed714640fa47cbe6c687370c032d6bf273c
pavan <107169500+pavan605@users.noreply.github.com>
2022-07-10 22:45:50 +0530

    Add files via upload

---
+++ string_cls1.py
@@ -0,0 +1,48 @@
+# a=7
+# print(float(a))
+# print(str(a))
+
+# a=10.3
+# print(int(a))
+# print(str(a))
+
+# x='hello'
+# print(int(x))
+# print(float(x))
+
+# a=10
+# print(a,type(a))
+# print(float(a))
+# print(str(a))
+
+# x=6
+# y='python'
+# print(y+str(x))
+# print(y+'   '+str(x))
+# print(y,x)
+# print(y+'         '+'version 3.10.5')
+
+
+# x='70'
+# print(x,type(x))
+# print(int(x))
+# print(float(x))
+
+# y='4.5'
+# print(y,type(y))
+# print(int(y))
+# print(float(y))
+# z=float(y)
+# print(int(z))
+
+
+# print('h'   +   'i')
+# print('friday'+' '+str(17)+'thjune')
+# print('july'+' '+str(18)+'my birthday' )
+
+'''my percentage in ug 75% '''
+from re import A
+
+
+g=75
+print('my percentage in ug',str(g)+'%')
